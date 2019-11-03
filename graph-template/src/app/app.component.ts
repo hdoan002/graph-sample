1b54b50b108f9c65dd01669b9a1e2cacecf0de16
Henry Doan <hdoan657@gmail.com>
2019-11-02 23:10:15 -0700

    Adding ngx-graph and attempting to implement. Trying ng-template

---
+++ graph-template/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'graph-template';
+
+  ngxNode =
+  [
+    {
+      id: 'first',
+      label: 'A'
+    }, {
+      id: 'second',
+      label: 'B'
+    }, {
+      id: 'third',
+      label: 'C'
+    }
+  ];
+
+  ngxLinks =
+  [
+    {
+      id: 'a',
+      source: 'first',
+      target: 'second',
+      label: 'is parent of'
+    }, {
+      id: 'b',
+      source: 'first',
+      target: 'third',
+      label: 'custom label'
+    }
+  ];
+}
